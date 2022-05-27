b820c93c5ea3069372eef292efd2f82cda2dafee
varnant <varnant@gmail.com>
2022-05-27 07:26:41 +0530

    Update One.java
    added  add() method created an instance o1 to call that method

--- One.java
+++ One.java
@@ -1,5 +1,10 @@
 class One{
+  void add(in a,int b){
+  System.out.println(a+b);
+  }
 public static void main(String ar[]){
 System.out.println("hello git 1");
+  One o1=new One();
+  o1.add(12,32);
 }
 }
